5019def7f532e7f145967197f8a7a737985e98c2
Jinchen Hou <64653117+jinchen-hou@users.noreply.github.com>
2020-05-13 13:19:51 -0400

    add lab3

---
+++ lab3/Flik/Flik.java
@@ -0,0 +1,8 @@
+/** An Integer tester created by Flik Enterprises. */
+public class Flik {
+    public static boolean isSameNumber(int a, int b) {
+        //Integer type has length limit!!
+        //Change it to int to fix the bug
+        return a == b;
+    }
+}
